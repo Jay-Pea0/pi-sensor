8967e2a4186a2fd42e85eed16792c46d2633be1e
Ben <benjaminpnicholls@gmail.com>
2022-05-19 01:20:14 +0100

    Changed log_error to log_entry. Added try block in send_data. Added function descriptions.

--- sensor_m.py
+++ sensor_m.py
@@ -1,69 +1,67 @@
+"""add in description
+"""
+
+
+import string
 from time import sleep
 from datetime import datetime
 from threading import Thread
 from gpiozero import MotionSensor
 
 
-def main():
-    # Configuration:
+def main() -> None:
+    # Configuration:  
     time_between_sending_data = 300
 
-    # End config
+    # End config.  
 
     motion_sensor_count = -1
     try:
-        motion_sensor = MotionSensor(1)  # Sensor plugged into GPIO###
+        motion_sensor = MotionSensor(1)  # Sensor plugged into GPIO###  
+        motion_sensor_count = 0  # Shows motion sensor is ready.  
     except MotionSensor.GPIOZeroError:
-        log_error("GPIOZero Error on initialising sensor")
-        end
+        log_entry("GPIOZero Error on initialising sensor")
+        quit
     except:
-        log_error("Error initialising sensor")
+        log_entry("Error initialising sensor")
+        quit
 
-    # Make a new thread to run every Nth minute to off-load the data 
+    # Make a new thread to run every Nth minute to off-load the data.  
     new_thread = Thread(target=send_data(motion_sensor_count, time_between_sending_data),args={})
     new_thread.start()
 
-    # Main loop.
+    # Main loop. Count each signal from the motion sensor.  
     while (True):
-        motion_sensor_count = start_sensors()
-        if motion_sensor_count < 0:  # Sensors are not started.
+        if motion_sensor_count == -1:  # Sensors are not started.  
             sleep(1)
             continue
         motion_sensor.wait_for_motion()
         motion_sensor_count += 1
 
-    
 
-
-def start_sensors():
-    # if no sensors attached, return, or try catch and log error?
-    # if sensors already active, return
-    # start sensors
-    # return
-    return 0
-
-
-def send_data(motion_sensor_count, time_between_sending_data):
+"""Send motion sensor data to XX. No return."""
+def send_data(motion_sensor_count: int, time_between_sending_data: int) -> None:
     if motion_sensor_count == -1: return
-    #do the send thing here
-    motion_sensor_count = 0
+    try:
+        #do the send thing here
+        motion_sensor_count = 0
+    except:
+        log_entry("failed logging count of ", motion_sensor_count)
     sleep(time_between_sending_data)
     return
 
 
-
-def log_error(error):
-    if error == None: return
-    if error == "": return
+"""Append a timestamp + message to the dated log file."""
+def log_entry(message: string) -> None:
+    if message == None: return
+    if message == "": return
     now = datetime.now()
     date = now.strftime("d/%m/%Y")
     nowtime = now.strftime("%H:%M:%S")
-    with open(date + " sensor error log", 'a') as log:
-        log.write(now + " " + error)
+    with open(date + " sensor log", 'a') as log:
+        log.write(now + " " + message)
     return
 
 
-
 if __name__ == '__main__':
-    print()
     main()
