f015824f666b706f21d172814c189df31a1879f6
Ben <benjaminpnicholls@gmail.com>
2022-05-19 19:32:50 +0100

    Added in command line arguments and their checks. Added comments to imports. Removed string import. Added getopt, requests, and sys imports. Added data dictionary for HTTP POST request. Now stores count too. Sensor initialisation (for IR or motion) added and made into separate functions. Sensor initialisation occurs per cycle (incase a sensor turns off etc).

--- sensor_m.py
+++ sensor_m.py
@@ -1,67 +1,136 @@
 """add in description
 """
 
+from sys import sys                 # Used for command line arguments.
+from getopt import getopt           # Used for parse command line arguments.
+from time import sleep              # Used for timing sending data.
+from datetime import datetime       # Used for log files.
+from threading import Thread        # Used for multi-threading (for timing).
+from requests import requests       # Used for HTML POST requests.
+from gpiozero import MotionSensor   # Used for motion sensor.
 
-from string import string
-from time import sleep
-from datetime import datetime
-from threading import Thread
-from gpiozero import MotionSensor
 
-
-def main() -> None:
-    # Configuration:  
-    time_between_sending_data = 300
-
+def main(argv) -> None:
+    # Defaults 
+    time_between_sending_data = 300  # 5 mins default.
+    pin = 1
+    data = {
+        "WEB_URL":"http://pastebin.com/api/api_post.php",
+        "KEY":"XXXXXXXXXXXXXXXXX",
+        "count":-1
+    }
     # End config.  
 
-    motion_sensor_count = -1
+    help_statement = "sensor.py -s <motion|ir> -i <interval/ms>"
     try:
-        motion_sensor = MotionSensor(1)  # Sensor plugged into GPIO###  
-        motion_sensor_count = 0  # Shows motion sensor is ready.  
-    except MotionSensor.GPIOZeroError:
-        log_entry("GPIOZero Error on initialising sensor")
-        quit
-    except:
-        log_entry("Error initialising sensor")
-        quit
+       opts, args = getopt.getopt(argv,"hs:p:i:",["sensor=","pin=","interval="])
+    except getopt.GetoptError:
+       print(help_statement)
+       sys.exit(2)
 
-    # Make a new thread to run every Nth minute to off-load the data.  
-    new_thread = Thread(target=send_data(motion_sensor_count, time_between_sending_data),args={})
+    for opt, arg in opts:
+        if opt == '-h':
+            print(help_statement)
+            sys.exit()
+        elif opt in ("-s", "--sensor"):
+            sensor = arg
+            if sensor == "motion": continue
+            elif sensor == "ir": continue
+            else:
+                print("Invalid sensor. ", help_statement)
+                sys.exit()
+        elif opt in ("-p", "--pin"):
+            pin = arg
+            if (pin < 1):
+                print("Pin set at command line too low: ", pin)
+        elif opt in ("-i", "--interval"):
+            interval = arg
+            if interval < 1:
+                print("Interval of ", interval, "is too short.")
+                sys.exit()
+            if interval > 600:
+                if input("Confirm interval of ", interval, "(y): ") != 'y':
+                    sys.exit() 
+
+    # Make a new thread to run every Nth second to off-load the data.  
+    new_thread = Thread(target=send_data(data, interval),args={})
     new_thread.start()
 
     # Main loop. Count each signal from the motion sensor.  
     while (True):
-        if motion_sensor_count == -1:  # Sensors are not started.  
+        # Check sensors are started, extra guarding.
+        if data["count"] == -1:
+            if sensor == "motion":
+                sensor, data = start_motion_sensor(data)
+            elif sensor == "ir":
+                sensor, data = start_ir_sensor(data)  
             sleep(1)
             continue
-        motion_sensor.wait_for_motion()
-        motion_sensor_count += 1
+        if sensor == "motion":
+            sensor.wait_for_motion()
+        elif sensor == "ir":
+            pass
+        data["count"] += 1
 
 
-"""Send motion sensor data to XX. No return."""
-def send_data(motion_sensor_count: int, time_between_sending_data: int) -> None:
-    if motion_sensor_count == -1: return
+"""Attempts to start the motion sensor.
+Initialises count to 0.
+Returns sensor and data dictionary.
+"""
+def start_motion_sensor(pin: int, data: dict) -> tuple(MotionSensor, dict):
     try:
-        #do the send thing here
-        motion_sensor_count = 0
+        sensor = MotionSensor(pin)  
+        data["count"] = 0  # Shows motion sensor is ready.  
+        log_entry("Initialised motion sensor on pin ", pin)
+    except MotionSensor.GPIOZeroError:
+        log_entry("GPIOZero Error on initialising sensor. Pin: ", pin)
+        sys.exit(2)
     except:
-        log_entry("failed logging count of ", motion_sensor_count)
-    sleep(time_between_sending_data)
+        log_entry("Error initialising sensor. Pin: ", pin)
+        sys.exit(2)
+    return sensor, data
+
+
+# Not implemented yet.
+"""Attempts to start the IR sensor.
+Initialises count to 0.
+Returns sensor and data dictionary.
+"""
+def start_ir_sensor(pin: int, data: dict) -> tuple(MotionSensor, dict):
+    try:
+        sensor = pin  # Sensor plugged into GPIO###  
+        data["count"] = 0  # Shows IR sensor is ready.  
+        log_entry("Initialised IR sensor on pin ", pin)
+    except:
+        log_entry("Error initialising sensor")
+        sys.exit(2)
+    return sensor, data
+
+
+"""Send sensor data to backend. No return."""
+def send_data(WEB_URL: str, data: dict, interval: int) -> None:
+    if data["count"] == -1: return  # Sensor not active.
+    try:
+        r = requests.post(url = WEB_URL, data = data)
+        log_entry(r)  # Response from the backend.
+        data["count"] = 0
+    except:
+        log_entry("Failed logging count of ", data["count"], "Will try again in ", interval, " seconds.")
+    sleep(interval)
     return
 
 
-"""Append a timestamp + message to the dated log file."""
-def log_entry(message: string) -> None:
+"""Append a timestamp + message to the dated log file. No return"""
+def log_entry(message: str) -> None:
     if message == None: return
     if message == "": return
     now = datetime.now()
-    date = now.strftime("d/%m/%Y")
-    nowtime = now.strftime("%H:%M:%S")
-    with open(date + " sensor log", 'a') as log:
-        log.write(now + " " + message)
+    now_date = now.strftime("d/%m/%Y")
+    now_time = now.strftime("%H:%M:%S")
+    with open(now_date + " sensor log", 'a') as log:
+        log.write(now_time + " " + message)
     return
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
